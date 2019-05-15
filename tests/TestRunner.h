7fb590be4ee691c17391c77024e78b8faea7b273
dais <daispaysagist@gmail.com>
2019-05-15 12:28:22 +0300

    fixes to TestRunner.

--- tests/TestRunner.h
+++ tests/TestRunner.h
@@ -12,38 +12,52 @@
 #include <cassert>
 #include <sstream>
 
-
+// for code conciseness
 using std::string;
 using std::cout;
 using std::endl;
 
-#define UNDERLINE "________________________________________________________________________________"
-#define OVERLINE  "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"
-
+// some text formatting tools
+#define UNDERLINE "______________________________________________________________________"
+#define OVERLINE  "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾"
 #define PASS "🗸"
 #define FAIL "❌"
 
-#define INIT_TESTS(is_verbose_macro_arg) \
-    bool verbose = (is_verbose_macro_arg); \
-    cout << UNDERLINE << endl << "Testing:  " << tester.extractFileNameFromFullPath(argv[0]); \
+// actual macros to use
+#define FILE_GLOBAL_INIT TestRunner tester
+#define MAIN_FUNC_INIT(is_verbose_macro_arg) \
+    bool verbose = (is_verbose_macro_arg), did_fail_at_least_1 = false, cur_test_result; \
+    string class_name = tester.extractFileNameFromFullPath(argv[0]); \
+    cout << UNDERLINE << endl << "Testing:  " <<  class_name;\
     if (verbose) { \
         cout << endl; \
         cout << OVERLINE << endl; \
+    } else { \
+        cout << " ... ";\
     }
 
-#define RUN_TEST(test_name) tester.runTest((test_name), #test_name, verbose)
+#define RUN_TEST(test_name) \
+        if (verbose) cout << endl << (#test_name) << " ... "; \
+        if (!tester.runTest((test_name), #test_name, verbose)) did_fail_at_least_1 = true;
+
 #define END_TESTS \
-    if (!verbose) { \
-        cout << " ... ️️" << PASS << endl << OVERLINE; \
+    if (verbose) { \
+        cout <<  OVERLINE << endl; \
     } \
     else { \
-        cout << endl << OVERLINE << endl; \
+        if (did_fail_at_least_1) { \
+            cout << class_name << "  " << FAIL; \
+        } else { \
+            cout << PASS; \
+        } \
+        cout << endl << OVERLINE; \
     }
 
 #define ASSERT_TRUE(expression) tester.assertTrue((expression), __LINE__, __FILE__)
 #define ASSERT_FALSE(expression) tester.assertFalse((expression), __LINE__, __FILE__)
 
 
+// the actual tester class
 class TestRunner {
 public:
     TestRunner() = default;
@@ -51,6 +65,10 @@ public:
     TestRunner(const TestRunner &) = delete;
 
     ~TestRunner() = default;
+
+    TestRunner operator=(const TestRunner &) = delete;
+
+    bool operator==(const TestRunner &) = delete;
 
     void assertTrue(const bool &expression, const int &line_num, const string &file_name) {
         if (!expression) throw AssertTrueIsFalseException(line_num, file_name);
@@ -62,16 +80,11 @@ public:
 
     bool runTest(const std::function<void()> &test_to_run, const string &test_name,
                  bool is_verbose) {
-        if (is_verbose) cout << test_name << " ... ";
         try {
             test_to_run();
         }
         catch (std::exception &e) {
-            if (is_verbose)
-                cout << FAIL << "  Caught exception:" << endl << e.what() << endl << endl;
-            else
-                cout << FAIL << "  In test " << test_name << "Caught exception:" << endl << e.what()
-                     << endl << endl;
+            cout << endl << FAIL << "  " << e.what() << endl;
             return false;
         }
         if (is_verbose) cout << PASS << endl;
@@ -87,40 +100,55 @@ public:
         return file_name;
     }
 
-    // exceptions
-    class AssertTrueIsFalseException : public std::exception {
+
+    // exception base class
+    class TestRunnerException : public std::exception {
     public:
-        AssertTrueIsFalseException(const int &line_num, const string &file_name) {
-            std::stringstream message_stream;
-            message_stream << "In file: " << file_name << endl <<"line " << line_num << endl <<
-                           "Expression should be true but it is false.";
-            this->message = message_stream.str();
+        TestRunnerException(const int &line_num, const string &file_name) {
+            this->message_stream << "In: " << file_name << ":" << line_num << ":" << endl << "    ";
+            this->common_message = this->message_stream.str();
+        }
+
+        const char *what() const noexcept override = 0;
+
+    protected:
+        std::stringstream message_stream;
+        string common_message;
+    };
+
+    // class-specific exception subclasses
+    class AssertTrueIsFalseException : public TestRunnerException {
+    public:
+        AssertTrueIsFalseException(const int &line_num, const string &file_name)
+                : TestRunnerException(line_num, file_name) {
+            this->message_stream << "Expression should be true but it is false.";
+            this->unique_message = this->message_stream.str();
         }
 
         const char *what() const noexcept override {
-            return this->message.c_str();
+            return this->unique_message.c_str();
         }
 
-    private:
-        string message;
+    protected:
+        string unique_message;
     };
 
-    class AssertFalseIsTrueException : public std::exception {
+    class AssertFalseIsTrueException : public TestRunnerException {
     public:
-        AssertFalseIsTrueException(const int &line_num, const string &file_name) {
-            std::stringstream message_stream;
-            message_stream << "In file: " << file_name << endl <<"line " << line_num << endl <<
-                           "Expression should be false but it is true.";
-            this->message = message_stream.str();
+        AssertFalseIsTrueException(const int &line_num, const string &file_name)
+                : TestRunnerException(line_num, file_name) {
+            this->message_stream << "Expression should be false but it is true.";
+            this->unique_message = this->message_stream.str();
         }
 
         const char *what() const noexcept override {
-            return this->message.c_str();
+            return this->unique_message.c_str();
         }
 
-    private:
-        string message;
+    protected:
+        string unique_message;
     };
+
 };
 
 #endif //INC_3DENGINE_TESTRUNNER_H
--- tests/test_Vec.cpp
+++ tests/test_Vec.cpp
@@ -5,23 +5,27 @@
 #include "../headers/Vec.h"
 #include "TestRunner.h"
 
-TestRunner tester;
+FILE_GLOBAL_INIT;
+
 
 void test_ctor_dtor() {
     Vec3d vec3d;
     ASSERT_TRUE(vec3d.x == 0.0 && vec3d.y == 0.0 && vec3d.z == 0.0);
-    ASSERT_FALSE(true);
+//    ASSERT_FALSE(true);
+//    ASSERT_TRUE(false);
 }
 
 void another_test() {
+//    ASSERT_TRUE(false);
 }
 
-void third_test(){
+void third_test() {
 }
+
 
 int main(int argc, char **argv) {
-    INIT_TESTS(true);
-    RUN_TEST(test_ctor_dtor);
+    MAIN_FUNC_INIT(true);
+    RUN_TEST(test_ctor_dtor)
     RUN_TEST(another_test);
     RUN_TEST(third_test);
     END_TESTS;
