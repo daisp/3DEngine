abee575e9d530bec9606f498a904959393669772
dais <daispaysagist@gmail.com>
2019-05-20 10:41:24 +0300

    removed cmake-build-debug directory

--- cmake-build-debug/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dais/CLionProjects/3DEngine/cmake-build-debug/googletest-src
-# Build directory: /home/dais/CLionProjects/3DEngine/cmake-build-debug/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
