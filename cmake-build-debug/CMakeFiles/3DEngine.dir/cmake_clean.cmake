cb5de203373442a1aab200385b1b9a4cbd5ae57e
dais <daispaysagist@gmail.com>
2019-05-20 10:42:47 +0300

    .gitignore fix

---
+++ cmake-build-debug/CMakeFiles/3DEngine.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/3DEngine.dir/source/main.cpp.o"
+  "CMakeFiles/3DEngine.dir/source/GraphicsEngine.cpp.o"
+  "CMakeFiles/3DEngine.dir/source/Renderer.cpp.o"
+  "CMakeFiles/3DEngine.dir/source/Mesh.cpp.o"
+  "CMakeFiles/3DEngine.dir/source/Matrix.cpp.o"
+  "CMakeFiles/3DEngine.dir/source/Triangle.cpp.o"
+  "CMakeFiles/3DEngine.dir/source/Line.cpp.o"
+  "3DEngine.pdb"
+  "3DEngine"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3DEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/test_Line.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_Line.dir/tests/gtest/test_Line.cpp.o"
+  "test_Line.pdb"
+  "test_Line"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_Line.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
